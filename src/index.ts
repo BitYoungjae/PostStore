ba8e8524669b3389900b4dffc9aa535174848448
Youngjae Joo <bityoungjae@gmail.com>
2020-07-01 23:02:47 +0900

    feat(index.ts): index.ts 생성

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import {
+  PostListPageProp,
+  PostPageProp,
+  getCategoryPageHandler,
+  getPageHandler,
+  getPostPageHandler,
+  getTagPageHandler,
+} from './pageHandler';
+
+export type { PostListPageProp, PostPageProp };
+
+export {
+  getCategoryPageHandler,
+  getPageHandler,
+  getPostPageHandler,
+  getTagPageHandler,
+};
