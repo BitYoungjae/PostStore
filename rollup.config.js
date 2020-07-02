da447ed184b59d49cffe48c14efbe0927153d040
Youngjae Joo <bityoungjae@gmail.com>
2020-07-03 02:23:25 +0900

    build(rollup.config.js): rollup 번들러 설정완료

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import pkg from './package.json';
+
+/** @type {import('rollup').RollupOptions} */
+const option = {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+    },
+  ],
+  external: [...Object.keys(pkg.dependencies || {}), 'fs', 'path', 'crypto'],
+  plugins: [
+    json(),
+    commonjs(),
+    typescript({
+      typescript: require('typescript'),
+      tsconfigOverride: {
+        compilerOptions: { module: 'ESNext' },
+      },
+    }),
+  ],
+};
+
+export default option;
