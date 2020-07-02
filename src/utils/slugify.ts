c18566a0a01bb7dc2980d0b546c75e6f7f8a5d0c
Youngjae Joo <bityoungjae@gmail.com>
2020-07-02 20:01:35 +0900

    fix(slugify): 유니코드 정규화 추가

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,8 +1,9 @@
 const spaceRegex = /\s+/g;
-const nonAllowedRegex = /[^가-힇a-z\d\s\.\-]/gi;
+const nonAllowedRegex = /[^가-힣a-z\d\s\.\-]/gi;
 
 const slugify = (name: string, delimeter = '-') => {
   const result = name
+    .normalize()
     .replace(nonAllowedRegex, '')
     .trim()
     .replace(spaceRegex, delimeter)
