c6eb23a49e6d8a0c177211b3a9399f6ae0fbcd6b
Youngjae Joo <bityoungjae@gmail.com>
2020-07-02 15:28:21 +0900

    feat(helpertypes): 자체 정의한 헬퍼타입을 위한 타입전용 모듈 생성

---
+++ src/utils/helperTypes.ts
@@ -0,0 +1,9 @@
+export interface ObjectMap<T> {
+  [key: string]: T;
+}
+
+export type SubType<BaseType, Key extends keyof BaseType> = BaseType[Key];
+export type SubTypeWithoutObjectMap<
+  BaseType,
+  Key extends keyof BaseType
+> = SubType<BaseType, Key> extends ObjectMap<infer R> ? R : never;
