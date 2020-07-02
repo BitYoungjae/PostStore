a428f64267842fc5e1637badde80e5a7d3c36956
Youngjae Joo <bityoungjae@gmail.com>
2020-07-02 20:25:41 +0900

    chore(getnodetree.test): 정렬 기능 스냅샷 테스트 추가

--- tests/getNodeTree.test.ts
+++ tests/getNodeTree.test.ts
@@ -1,6 +1,10 @@
 import { testPath } from './lib/env';
-import { getNodeTree } from '../src/utils/getNodeTree';
+import { getNodeTree, FileNode } from '../src/utils/getNodeTree';
 import { snapShotTest } from './lib/snapshotTest';
+import { getPostsByCategories } from '../src/common';
+
+const onlyFileName = (nodeList: FileNode[]) =>
+  nodeList.map((node) => node.name.normalize());
 
 test('getNodeTree', async () => {
   const fileTree = await getNodeTree({
@@ -8,5 +12,28 @@ test('getNodeTree', async () => {
   });
 
   const testResult = await snapShotTest(fileTree, 'fileTree');
+  expect(testResult).toBe(true);
+});
+
+test('sort test', async () => {
+  const fileTree = await getNodeTree({
+    nodePath: testPath,
+  });
+
+  const sortByDate = onlyFileName(
+    getPostsByCategories(fileTree, ['테스트용-게시물들', '날짜순-정렬']),
+  );
+  const sortByName = onlyFileName(
+    getPostsByCategories(fileTree, ['테스트용-게시물들', '제목순-정렬']),
+  );
+  const sortByComplex = onlyFileName(
+    getPostsByCategories(fileTree, ['테스트용-게시물들', '복합-정렬']),
+  );
+
+  const testResult = await snapShotTest(
+    { sortByDate, sortByName, sortByComplex },
+    'sortTest',
+  );
+
   expect(testResult).toBe(true);
 });
