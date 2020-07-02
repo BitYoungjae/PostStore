b28bf1fcb7b2e744f8746c493b4bfbba3dde527c
Youngjae Joo <bityoungjae@gmail.com>
2020-07-02 20:10:44 +0900

    feat(getnodetree): 일괄된 카테고리 및 게시물 순서를 유지하기 위한 정렬 기능 추가

--- src/utils/getNodeTree.ts
+++ src/utils/getNodeTree.ts
@@ -3,6 +3,7 @@ import path from 'path';
 
 import { PostData, parsePost } from '../postParser';
 import { slugify } from './slugify';
+import { isCategoryNode, isPostNode } from '../common';
 
 const fsPromise = fs.promises;
 const markdownRegex = new RegExp(/\.mdx?$/);
@@ -39,7 +40,7 @@ export async function getNodeTree({
   nodePath,
   isFile = false,
 }: getNodeTreeProps): Promise<FileNode> {
-  const nodeName = path.basename(nodePath);
+  const nodeName = path.basename(nodePath).normalize();
 
   const newNode: FileNode = {
     type: isFile ? 'post' : 'category',
@@ -57,7 +58,10 @@ export async function getNodeTree({
 
   newNode.children = [];
 
-  const nodeList = await fsPromise.readdir(nodePath, { withFileTypes: true });
+  const nodeList = await fsPromise.readdir(nodePath, {
+    withFileTypes: true,
+    encoding: 'utf-8',
+  });
   if (!nodeList.length) return newNode;
 
   for (const node of nodeList) {
@@ -75,5 +79,37 @@ export async function getNodeTree({
     }
   }
 
+  newNode.children = sortChildren(newNode.children);
   return newNode;
 }
+
+const sortChildren = (nodeList: FileNode[]): FileNode[] => {
+  const categoryList = nodeList.filter(isCategoryNode);
+  const postList = nodeList.filter(isPostNode);
+
+  categoryList.sort(({ name: aName }, { name: bName }) =>
+    aName.localeCompare(bName),
+  );
+  postList.sort(sortRule);
+
+  return [...categoryList, ...postList];
+};
+
+const sortRule = (a: PostNode, b: PostNode): number => {
+  const dateDiff = b.postData.date - a.postData.date; // 날짜 기준 내림차순 정렬
+
+  // 같은 날짜인 경우 게시물 제목 기준으로 정렬
+  if (dateDiff === 0) {
+    const aTitle = a.postData.title.toLowerCase();
+    const bTitle = b.postData.title.toLowerCase();
+
+    // 첫 글자만 비교
+    const firstCharDiff = aTitle[0].localeCompare(bTitle[0]);
+    // 첫 글자도 같을 경우 전체 비교
+    if (firstCharDiff === 0) return aTitle.localeCompare(bTitle);
+
+    return firstCharDiff;
+  }
+
+  return dateDiff;
+};
