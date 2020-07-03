bb8dc199c7e84f8135fb4e2dd29ff770aa427b8a
Youngjae Joo <bityoungjae@gmail.com>
2020-07-04 00:23:53 +0900

    refactor: 불필요한 타입 제거

--- src/utils/helperTypes.ts
+++ src/utils/helperTypes.ts
@@ -2,12 +2,10 @@ export interface ObjectMap<T> {
   [key: string]: T;
 }
 
-export type SubType<BaseType, Key extends keyof BaseType> = BaseType[Key];
-
 export type SubTypeWithoutObjectMap<
   BaseType,
   Key extends keyof BaseType
-> = SubType<BaseType, Key> extends ObjectMap<infer R> ? R : never;
+> = BaseType[Key] extends ObjectMap<infer R> ? R : never;
 
 export type Clone<BaseType extends string, ValueType> = {
   [key in BaseType]: ValueType;
