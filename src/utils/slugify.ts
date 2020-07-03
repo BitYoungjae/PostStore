d9ec1362e464392fb276510ba3a447b68fabb586
Youngjae Joo <bityoungjae@gmail.com>
2020-07-03 09:22:10 +0900

    feat(slugify): error handling 추가 / 대소문자 변환 삭제

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,3 +1,5 @@
+import { getStyledErrorMsg } from '../common';
+
 const spaceRegex = /\s+/g;
 const nonAllowedRegex = /[^가-힣a-z\d\s\.\-]/gi;
 
@@ -6,8 +8,15 @@ const slugify = (name: string, delimeter = '-') => {
     .normalize()
     .replace(nonAllowedRegex, '')
     .trim()
-    .replace(spaceRegex, delimeter)
-    .toLowerCase();
+    .replace(spaceRegex, delimeter);
+
+  if (!result)
+    throw new Error(
+      getStyledErrorMsg(
+        `The name cannot be slugified by the slugify module.`,
+        `name : ${name}`,
+      ),
+    );
 
   return result;
 };
