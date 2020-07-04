90995c347ceaf3705a1df0eda8fcdadf0b5b9a66
Youngjae Joo <bityoungjae@gmail.com>
2020-07-05 06:08:23 +0900

    chore: 메시지 아이콘 변경

--- src/lib/msgHandler.ts
+++ src/lib/msgHandler.ts
@@ -1,11 +1,11 @@
 import chalk from 'chalk';
 
 export const getStyledErrorMsg = (msg: string, inputValue?: string) =>
-  chalk`🌶{red.bold ERROR:} {yellow.bold ${msg}}{green.bold ${
+  chalk`🍎 {red.bold ERROR:} {yellow.bold ${msg}}{green.bold ${
     inputValue ? ` (${inputValue})` : ''
   }}`;
 
 export const getStyledInfoMsg = (msg: string, inputValue?: string) =>
-  chalk`🥬{blue.bold INFO} {green.bold ${msg}}{yellow.bold ${
+  chalk`🥬 {blue.bold INFO} {green.bold ${msg}}{yellow.bold ${
     inputValue ? ` (${inputValue})` : ''
   }}`;
