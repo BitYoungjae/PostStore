26e9438d641aa84ff7a9dbbf28135593008e0ae1
Youngjae Joo <bityoungjae@gmail.com>
2020-07-05 19:39:08 +0900

    feat: 오류 / 주의 / 정보로 메시지 유형 분류

--- src/lib/msgHandler.ts
+++ src/lib/msgHandler.ts
@@ -1,11 +1,16 @@
 import chalk from 'chalk';
 
 export const getStyledErrorMsg = (msg: string, inputValue?: string) =>
-  chalk`🍎 {red.bold ERROR:} {yellow.bold ${msg}}{green.bold ${
+  chalk`🍎 {red.bold ERROR:} {yellow.bold ${msg}}{red.bold ${
+    inputValue ? ` (${inputValue})` : ''
+  }}`;
+
+export const getStyledCautionMsg = (msg: string, inputValue?: string) =>
+  chalk`🍋 {yellow.bold ERROR:} {magenta.bold ${msg}}{yellow.bold ${
     inputValue ? ` (${inputValue})` : ''
   }}`;
 
 export const getStyledInfoMsg = (msg: string, inputValue?: string) =>
-  chalk`🥬 {blue.bold INFO} {green.bold ${msg}}{yellow.bold ${
+  chalk`🥬 {green.bold INFO} {yellow.bold ${msg}}{green.bold ${
     inputValue ? ` (${inputValue})` : ''
   }}`;
